0249d1501d54aa3ef36112e2e55366712e0a7f8d
2conca <riopisces.pqh@gmail.com>
2024-05-23 19:15:19 +0700

    30daysFrontend

--- Ecommerce/ecommerce/src/Component/NavbarComponent/NavbarComponent.jsx
+++ Ecommerce/ecommerce/src/Component/NavbarComponent/NavbarComponent.jsx
@@ -1,9 +1,22 @@
 import React from "react";
-import { NavbarHeader } from "./style";
-
+import "../NavbarComponent/NavbarComponent.scss";
 class NavbarComponent extends React.Component {
     render() {
-        return <NavbarHeader></NavbarHeader>;
+        return (
+            <div className="NavbarHeader">
+                <div className="LeftNavHeader">
+                    <a href=" ">Rao vặt</a>
+                    <a href=" ">Tư vấn</a>
+                    <a href=" ">Hỏi đáp</a>
+                    <a href=" ">Hỗ trợ</a>
+                </div>
+                <div className="RightNavHeader">
+                    <a href=" ">Giỏ hàng</a>
+                    <a href=" ">Thông báo</a>
+                    <a href=" ">Phan Quang Huy</a>
+                </div>
+            </div>
+        );
     }
 }
 
--- Ecommerce/ecommerce/src/Component/NavbarComponent/style.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const NavbarHeader = styled.div`
-    height: 40px;
-    background-color: white;
-    color: #767676;
-`;
